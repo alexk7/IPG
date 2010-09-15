de216bed92f7634d6f7908982eab46a1a050e2c7
Alexandre Cossette <alex.cossette@gmail.com>
2010-09-15 13:35:41 -0400

    added empty GenerateParseTreeIterator files

---
+++ cpp/GenerateParseTreeIterator.cpp
@@ -0,0 +1,11 @@
+/*
+ *  GenerateParseTreeIterator.cpp
+ *  ipg
+ *
+ *  Created by Alexandre Cossette on 15-09-10.
+ *  Copyright 2010 alexk7. All rights reserved.
+ *
+ */
+
+#include "GenerateParseTreeIterator.h"
+
---
+++ cpp/GenerateParseTreeIterator.h
@@ -0,0 +1,9 @@
+/*
+ *  GenerateParseTreeIterator.h
+ *  ipg
+ *
+ *  Created by Alexandre Cossette on 15-09-10.
+ *  Copyright 2010 alexk7. All rights reserved.
+ *
+ */
+
